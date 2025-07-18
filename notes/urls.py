70ad70fa1e432658d94dd7bb0521b57eb34ec5ba
Deotyma <kalistaewa@gmail.com>
2025-07-18 23:41:53 +0200

    delete added

--- notes/urls.py
+++ notes/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('notes/popular/', views.PopularNoteListView.as_view()),
     path('notes/<int:pk>/', views.NoteDetailView.as_view(), name='notes.detail'),
     path('notes/<int:pk>/edit', views.NoteUpdateView.as_view(), name='notes.edit'),
+    path('notes/<int:pk>/delete', views.NoteDeleteView.as_view(), name='notes.delete'),
     path('notes/create/', views.NoteCreateView.as_view(), name='notes.create'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView, UpdateView
+from django.views.generic.edit import DeleteView
 
 from.models import Note
 from .forms import NoteForm
+
+class NoteDeleteView(DeleteView):
+    """
+    Class-based view for deleting a note.
+    """
+    model = Note
+    template_name = 'notes/notes_delete.html'
+    success_url = '/smart/notes'  # Redirect to the list of notes after deletion
+
 
 class NoteUpdateView(UpdateView):
     """
     Class-based view for updating an existing note.
     """
     model = Note
-    template_name = 'notes/notes_update.html'
+    template_name = 'notes/notes_create.html'
     success_url = '/smart/notes'  # Redirect to the list of notes after update
     form_class = NoteForm  # Use the NoteForm defined in forms.py
 

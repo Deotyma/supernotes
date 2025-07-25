0906a45a3ed77204a4e88a29155884dc9ef9bd22
Deotyma <kalistaewa@gmail.com>
2025-07-25 15:53:47 +0200

    error in home page corriged

--- smartnotes/urls.py
+++ smartnotes/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
+     path(
+        '', 
+        TemplateView.as_view(template_name='home/welcome.html'),
+        name='welcome'
+    ),
     path('admin/', admin.site.urls),
     path('home/',include('home.urls')),
     path('smart/', include('notes.urls'))

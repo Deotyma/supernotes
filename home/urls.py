742d3d78cbbf7fc5df0821df90dd1285e2f16657
Deotyma <kalistaewa@gmail.com>
2025-07-25 10:21:28 +0200

    login and register added

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
 from . import views
 
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('login/', views.LoginInterfaceLoginView.as_view(), name='login'),
-    path('logout/', views.LoginInterfaceLogoutView.as_view(), name='logout'),
+    path('logout/', views.LogoutInterfaceView.as_view(), name='logout'),
+    path('register/', views.SignupView.as_view(), name='register'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 class HomeView(TemplateView):
     """
@@ -16,5 +17,16 @@ class LoginInterfaceLoginView(LoginView):
     template_name = 'home/login.html'
 
 
-class LoginInterfaceLogoutView(LogoutView):
+class LogoutInterfaceView(LogoutView):
     template_name = 'home/logout.html'
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
+    
